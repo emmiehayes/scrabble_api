6dabe83f99a6bb4d94da12c15195819c59855ac0
Emmie Hayes <emmiehayes2@gmail.com>
2018-09-12 13:51:34 -0600

    adjust method hitting api- getting objects back in view

--- app/models/word.rb
+++ app/models/word.rb
@@ -6,12 +6,12 @@ class Word
 
   def validate  
     @conn = Faraday.new(url: "https://od-api.oxforddictionaries.com") do |faraday|
-      faraday.headers["app_key"] = ENV["api_key"]
-      faraday.headers["app_id"] = ENV["app_id"]
-      faraday.headers["accept"] = "application/json"
+      faraday.headers["accept"] = 'application/json'
+      faraday.headers["app_id"] = ENV['app_id']
+      faraday.headers["app_key"] = ENV['app_key']
       faraday.adapter Faraday.default_adapter
     end
-    @conn.get("/inflections/{source_lang}/{word_id}") 
+    response = @conn.get("/api/v1/inflections/#{source_lang}/#{word_id}")
     dictionary_word = JSON.parse(response.body, symbolize_names: true)[:results]
   end
 
