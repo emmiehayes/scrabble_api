c4acc623730d48b2683cd50d77fb241258a452e4
Emmie Hayes <emmiehayes2@gmail.com>
2018-09-12 13:33:11 -0600

    bdd from view, moving to search presenter WIP

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,11 @@
 class SearchController < ApplicationController
   def index 
+    @presenter = SearchPresenter.new(search_params)
+  end
+
+  private 
+
+  def search_params
+    params.permit(:word)
   end
 end
