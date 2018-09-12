597ac9cb2aa1d7ffb1df9a3c6838565291395967
Emmie Hayes <emmiehayes2@gmail.com>
2018-09-12 15:07:42 -0600

    taking a break

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,3 +1,5 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id,:player_1_id, :player_2_id
+  attributes :id, :player_1_id, :player_2_id
 end
+
+#leaving off here for now- need to pull in attributes for user based on player as well as the player/user.score
